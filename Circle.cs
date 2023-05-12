a517c454b458e6a8f3189b8aa2470ac87f2d0c1c
O_Riley <86636387+Randumbdude@users.noreply.github.com>
2023-05-11 19:50:58 -0500

    Beta Revamp

---
+++ Circle.cs
@@ -0,0 +1,30 @@
+﻿namespace Gravity_Simulator
+{
+    class Circle
+    {
+        public Vector2 position; //top left
+        public Vector2 centerLocation;
+        public float radius;
+        public float width;
+        public float height;
+
+        public Circle() { }
+
+        public Circle(Vector2 position, float radius, float width, float height)
+        {
+            this.position = position;
+            this.centerLocation = new Vector2(position.x + (width / 2), position.y + (height / 2));
+            this.radius = radius;
+            this.width = width;
+            this.height = height;
+        }
+
+        //get center position?
+        /*
+        
+        centerpositionX = positionX + (width / 2)
+        centerpositionY = positionY + (height / 2)
+
+        */
+    }
+}
