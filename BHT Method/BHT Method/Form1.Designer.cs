03f74eafefcfcf5af8fb176bddfb5ff8c7329488
O_Riley <86636387+Randumbdude@users.noreply.github.com>
2023-05-25 21:23:46 -0500

    Add files via upload

---
+++ BHT Method/BHT Method/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace BHT_Method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.simBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simBox
+            // 
+            this.simBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.simBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.simBox.Location = new System.Drawing.Point(12, 12);
+            this.simBox.Name = "simBox";
+            this.simBox.Size = new System.Drawing.Size(1213, 666);
+            this.simBox.TabIndex = 0;
+            this.simBox.TabStop = false;
+            this.simBox.Paint += new System.Windows.Forms.PaintEventHandler(this.simBox_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 690);
+            this.Controls.Add(this.simBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.simBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox simBox;
+    }
+}
+
